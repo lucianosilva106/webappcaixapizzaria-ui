01cd3c01200c6c9b29540f3a3d83e6cadc88b664
Luciano Ferreira <l.ferreira1975@hotmail.com>
2021-05-11 13:49:28 -0300

    uma tela simples webappcaixapizzaria ui

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const HelloWorld = (props) => (
+  <div>
+    <h1>Hello {props.name}</h1>
+  </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+<HelloWorld name="Mateus"/>,
+  document.getElementById('root')
+);
