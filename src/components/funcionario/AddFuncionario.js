01062af56e90713a4809ec2ab28d8cb79b8b7ad8
Luciano Ferreira <l.ferreira1975@hotmail.com>
2021-05-14 10:43:12 -0300

    POST deu certo

--- src/components/funcionario/AddFuncionario.js
+++ src/components/funcionario/AddFuncionario.js
@@ -1,83 +1,74 @@
-import { Alert } from 'bootstrap';
 import React from 'react';
-//import { ToastContainer, toast } from 'react-toastify';
-//import 'react-toastify / dist / ReactToastify.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export class AddFuncionario extends React.Component {
     constructor(props) {
         super();
         this.state = {
-            id: '',
+            id: 0,
             fun_nome: '',
             fun_chapeira: '',
             fun_login: '',
             fun_senha: ''
         };
 
-//        this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-//    handleChange(evento) {
-//        this.setState({id: evento.target.id});
-//        this.setState({fun_nome: evento.target.fun_nome});
-//        this.setState({fun_chapeira: evento.target.fun_chapeira});
-//        this.setState({fun_login: evento.target.fun_login});
-//        this.setState({fun_senha: evento.target.fun_senha});
-//        evento.preventDefault();
-//    }
 
     async handleSubmit(evento) {
-        alert('Um formulario foi enviado:' + JSON.stringify(this.state));
+        const data = JSON.stringify(this.state)
+        alert('Um formulario foi enviado:' + data);
         evento.preventDefault();
 
         fetch('https://localhost:44331/api/funcionarios', {
            method: 'POST',
-            body: JSON.stringify(this.state) 
+           headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+           },
+            body: data
         }).then(function(response) {
-            console.log(response)
+            toast.success('Dados cadastrados com sucesso')
             return response.json();
-        });
+        }).catch(function(erro) {
+            toast.error('Erro ao cadastrar')
+        })
     }
 
     render() {
         return(
         <div class="container">
-            <div class="card mx-auto shadow -lg p-3 mb-5 bg-white rounded animate__animated animate__zoomIn">
+            <ToastContainer />
+            <div class="card mx-auto shadow -lg p-3 mb-5 bg-white rounded animate_animated animate_zoomIn">
             <form onSubmit={this.handleSubmit}>
                 <label>
-                Id:
-                    <input 
-                        name="id"
-                        type="number"
-                        onChange={e => this.setState({id: e.target.value})}
-                    />
-                    <br />
                 Nome:
-                    <input 
+                    <input
                         name="fun_nome"
-                        type="text" 
+                        type="text"
                         onChange={e => this.setState({fun_nome: e.target.value})}
                     />
                     <br />
                 Chapeira:
-                    <input 
+                    <input
                         name="fun_chapeira"
-                        type="text" 
+                        type="text"
                         onChange={e => this.setState({fun_chapeira: e.target.value})}
                     />
                     <br />
                 Login:
-                    <input 
+                    <input
                         name="fun_login"
-                        type="text" 
+                        type="text"
                         onChange={e => this.setState({fun_login: e.target.value})}
                     />
                     <br />
                 Senha:
-                    <input 
+                    <input
                         name="fun_senha"
-                        type="password" 
+                        type="password"
                         onChange={e => this.setState({fun_senha: e.target.value})}
                     />
                     <br />
