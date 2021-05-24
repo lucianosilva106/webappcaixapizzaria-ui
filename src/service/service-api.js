7d57d403f32df91529722a033f58eb13827ed41b
Luciano Ferreira <l.ferreira1975@hotmail.com>
2021-05-24 12:59:52 -0300

    url alterada

--- src/service/service-api.js
+++ src/service/service-api.js
@@ -1,2 +1,3 @@
-const URL_API = process.env.URL_API||'https://localhost:44331'
+//const URL_API = process.env.URL_API||'https://localhost:44331'
+const URL_API = 'https://webappcaixapizzaria-api.azurewebsites.net'
 export default URL_API
